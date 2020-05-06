5378faf7a90cfcab9ff831afb796d47a1217a7d8
Lancelot Lachartre <lancelotlachartre@Lancelots-Personal-MacBook-Pro.local>
2020-05-06 19:12:31 +0100

    Bug fix

--- pyweather/pyweather/utils/api_keys.py
+++ pyweather/pyweather/utils/api_keys.py
@@ -5,6 +5,6 @@ def find_key(key_name):
     """Attempts to find the following API key in the environment variables"""
     value = os.getenv(key_name)
     if not value:
-        raise MissingAPIKey({"api_key": api_key})
+        raise MissingAPIKey({"api_key": key_name})
     else:
         return value
