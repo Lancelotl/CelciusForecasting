c302b7497592bef7cb8fe792d91e5ac11b1512f5
Lancelot Lachartre <lancelotlachartre@Lancelots-Personal-MacBook-Pro.local>
2020-05-24 17:33:39 +0100

    Better path support

--- pyweather/pyweather/__init__.py
+++ pyweather/pyweather/__init__.py
@@ -4,9 +4,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 from . import exceptions
 
-parent = Path(".")
-env_path = parent / "variables.env"
-load_dotenv(dotenv_path=env_path)
+PROJECT_PATH = os.getenv("PROJECT_PATH")
+if PROJECT_PATH:
+    env_variables_filepath = Path(PROJECT_PATH) / "variables.env"
+else:
+    parent = Path(".")
+    env_variables_filepath = parent / "variables.env"
+
+load_dotenv(dotenv_path=env_variables_filepath)
 
 api_keys = [
     "ACCUWEATHER_API_KEY",
